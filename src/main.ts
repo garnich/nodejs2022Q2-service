b0b0720a1fc452889dfd5deeee47806cc576184b
Andrei Ihnatsiuk <andrei_ihnatsiuk@epam.com>
2022-07-17 14:59:10 +0300

    updated PORT injection

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,14 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const PORT = process.env.PORT || 4000;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(
     new ValidationPipe({ whitelist: true })
   )
-  await app.listen(4000);
+  await app.listen(PORT);
 }
 bootstrap();
