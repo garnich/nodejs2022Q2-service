41ff907466bd4f576934cf31ee9cb2dbb8f0a98b
Andrei Ihnatsiuk <andrei_ihnatsiuk@epam.com>
2022-07-17 13:44:02 +0300

    updates favorites controller structure

--- src/favorites/favorites.controller.ts
+++ src/favorites/favorites.controller.ts
@@ -30,77 +30,101 @@ export class FavoritesController {
     @Header('Accept', 'application/json')
     @HttpCode(HttpStatus.CREATED)
     addTrackToFavorite(@Param('id', new ParseUUIDPipe({ version: '4' })) id: string): ITracks {
-        if(!IDValidator(id)) throw invalidIdExeption();
-
-        const track: ITracks = this.trackService.getTrack(id);
-
-        if(!track) throw itemNotExistExeption('track');
-
-        return this.favoritesService.addTrackToFavorites(track);
+        if(!IDValidator(id)) {
+            throw invalidIdExeption();
+        } else {
+            const track: ITracks = this.trackService.getTrack(id);
+            
+            if(!track) {
+                throw itemNotExistExeption('track');
+            } else {
+                return this.favoritesService.addTrackToFavorites(track);
+            }
+        }
     }
 
     @Post('artist/:id')
     @Header('Accept', 'application/json')
     @HttpCode(HttpStatus.CREATED)
     addArtistToFavorite(@Param('id', new ParseUUIDPipe({ version: '4' })) id: string): IArtists {
-        if(!IDValidator(id)) throw invalidIdExeption();
-
-        const artist: IArtists = this.artistService.getArtist(id);
-
-        if(!artist) throw itemNotExistExeption('artist');
-
-        return this.favoritesService.addArtistToFavorites(artist);
+        if(!IDValidator(id)) {
+            throw invalidIdExeption();
+        } else {
+            const artist: IArtists = this.artistService.getArtist(id);
+            
+            if(!artist) {
+                throw itemNotExistExeption('artist');
+            } else {
+                return this.favoritesService.addArtistToFavorites(artist);
+            }
+        }
     }
 
     @Post('album/:id')
     @Header('Accept', 'application/json')
     @HttpCode(HttpStatus.CREATED)
     addAlbumToFavorite(@Param('id', new ParseUUIDPipe({ version: '4' })) id: string): IAlbums {
-        if(!IDValidator(id)) throw invalidIdExeption();
-
-        const album: IAlbums = this.albumService.getAlbum(id);
-
-        if(!album) throw itemNotExistExeption('album');
-
-        return this.favoritesService.addAlbumToFavorites(album);
+        if(!IDValidator(id)) {
+            throw invalidIdExeption();
+        } else {
+            const album: IAlbums = this.albumService.getAlbum(id);
+            
+            if(!album) {
+                throw itemNotExistExeption('album');
+            } else {
+                return this.favoritesService.addAlbumToFavorites(album);
+            }
+        }
     }
 
     @Delete('track/:id')
     @Header('Accept', 'application/json')
     @HttpCode(HttpStatus.NO_CONTENT)
     deleteTrack(@Param('id', new ParseUUIDPipe({ version: '4' })) id: string){
-        if(!IDValidator(id)) throw invalidIdExeption();
-
-        const isTrackExist: boolean = !!this.trackService.getTrack(id);
-
-        if(!isTrackExist) throw itemNotExistExeption('track');
-
-        return this.favoritesService.removeTrackFromFavorites(id);
+        if(!IDValidator(id)) {
+            throw invalidIdExeption();
+        } else {
+            const isTrackExist: boolean = !!this.trackService.getTrack(id);
+            
+            if(!isTrackExist) {
+                throw itemNotExistExeption('track');
+            } else {
+                return this.favoritesService.removeTrackFromFavorites(id);
+            }
+        }
     }
 
     @Delete('artist/:id')
     @Header('Accept', 'application/json')
     @HttpCode(HttpStatus.NO_CONTENT)
     deleteArtist(@Param('id', new ParseUUIDPipe({ version: '4' })) id: string){
-        if(!IDValidator(id)) throw invalidIdExeption();
-
-        const isArtistExist: boolean = !!this.artistService.getArtist(id);
-
-        if(!isArtistExist) throw itemNotExistExeption('artist');
-
-        return this.favoritesService.removeArtistFromFavorites(id);
+        if(!IDValidator(id)) {
+            throw invalidIdExeption();
+        } else {
+            const isArtistExist: boolean = !!this.artistService.getArtist(id);
+            
+            if(!isArtistExist) {
+                throw itemNotExistExeption('artist');
+            } else {
+                return this.favoritesService.removeArtistFromFavorites(id);
+            }
+        }
     }
 
     @Delete('album/:id')
     @Header('Accept', 'application/json')
     @HttpCode(HttpStatus.NO_CONTENT)
     deleteAlbum(@Param('id', new ParseUUIDPipe({ version: '4' })) id: string){
-        if(!IDValidator(id)) throw invalidIdExeption();
-
-        const isAlbumtExist: boolean = !!this.albumService.getAlbum(id);
-
-        if(!isAlbumtExist) throw itemNotExistExeption('album');
-
-        return this.favoritesService.removeAlbumFromFavorites(id);
+        if(!IDValidator(id)) {
+            throw invalidIdExeption();
+        } else {
+            const isAlbumtExist: boolean = !!this.albumService.getAlbum(id);
+            
+            if(!isAlbumtExist) {
+                throw itemNotExistExeption('album');
+            } else {
+                return this.favoritesService.removeAlbumFromFavorites(id);
+            }
+        }
     }
 }
