e7eb85ebab1c63cbe879cb8cbb3ddf4381d6af86
Andrei Ihnatsiuk <andrei_ihnatsiuk@epam.com>
2022-07-22 00:43:29 +0300

    connected to typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AlbumsModule } from './albums/albums.module';
 import { ArtistsModule } from './artists/artists.module';
 import { TracksModule } from './tracks/tracks.module';
@@ -7,6 +8,7 @@ import { UsersModule } from './users/user.module';
 import { FavoritesModule } from './favorites/favorites.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserEntity } from './entities/user.entitiy';
 
 @Module({
   imports: [
@@ -16,6 +18,18 @@ import { AppService } from './app.service';
     UsersModule,
     FavoritesModule,
     ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'postgres',
+      port: +(process.env.POSTGRES_PORT as string) as number,
+      username: process.env.POSTGRES_USER as string,
+      password: process.env.POSTGRES_PASSWORD as string,
+      database: process.env.POSTGRES_DB as string,
+      entities: [__dirname + '/**/entites/*.entity{.ts,.js}'],
+      logging: true,
+      autoLoadEntities: true,
+      synchronize: true,
+    })
   ],
   controllers: [AppController],
   providers: [AppService],
